a52faee8baa9afdfbc414051161b03980bb143c7
RamiEchary <rami@boulders.dk>
2025-09-20 00:33:37 +0200

    Update vite config and trigger deployment

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,12 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  base: '/bouldersnetwork/',
-})
+  base: mode === 'production' ? '/bouldersnetwork/' : '/',
+}))
